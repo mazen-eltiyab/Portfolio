75e2e47935ca53e5e85cd482e6993ec490d98865
mazen-eltiyab <mazinnn2006@gmail.com>
2026-03-29 05:45:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 70, // لضبط المسافة مع شريط التنقل
+            behavior: 'smooth'
+        });
+    });
+});
